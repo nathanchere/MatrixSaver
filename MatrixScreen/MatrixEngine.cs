36191017284505cbeff9df5331fa6015dacfb608
Nathan Chere <git@nathanchere.com.au>
2014-04-24 16:13:05 +1000

    Global boundary debug info

--- MatrixScreen/MatrixEngine.cs
+++ MatrixScreen/MatrixEngine.cs
@@ -29,11 +29,19 @@ namespace MatrixScreen
                 text.DisplayedString = string.Format("Cursor: {0}:{1}", Mouse.GetPosition().X, Mouse.GetPosition().Y);
                 viewport.Window.Draw(text);
 
-                text.Position = new Vector2f(30, 60);
+                text.Position = new Vector2f(30, 44);
                 text.DisplayedString = string.Format("Viewport #{0}; origin: {1},{2}",
                     viewport.ID,
                     viewport.WorkingArea.Top,
                     viewport.WorkingArea.Left);
+                viewport.Window.Draw(text);
+
+                text.Position = new Vector2f(30, 58);
+                text.DisplayedString = string.Format("Global boundaries: t:{0},l:{1},r:{2},b{3}",
+                    _viewports.WorkingArea.Top,
+                    _viewports.WorkingArea.Left,
+                    _viewports.WorkingArea.Right,
+                    _viewports.WorkingArea.Bottom);
                 viewport.Window.Draw(text);
 
                 viewport.Window.Display();
