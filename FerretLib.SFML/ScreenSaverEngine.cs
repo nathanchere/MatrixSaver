f7eeca19e113132f0d712e41292a1d4ab613b146
Nathan Chere <git@nathanchere.com.au>
2014-04-28 13:57:37 +1000

    Rendering multi screen fixed

--- FerretLib.SFML/ScreenSaverEngine.cs
+++ FerretLib.SFML/ScreenSaverEngine.cs
@@ -21,6 +21,7 @@ namespace FerretLib.SFML
         {
             _viewPorts = new ViewPortCollection(true, true);
             _canvas = new RenderTexture((uint) _viewPorts.WorkingArea.Width, (uint) _viewPorts.WorkingArea.Height, false);
+            _canvas.Display(); // Needed due to FBO causing inverted co-ords otherwise
             _chrono = new Chrono();
         }
 
