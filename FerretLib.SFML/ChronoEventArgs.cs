b1198d976805c6ec3a1570b2ee63c358604870f6
Nathan Chere <git@nathanchere.com.au>
2014-04-29 18:18:30 +1000

    Add datetime to chronoeventargs

--- FerretLib.SFML/ChronoEventArgs.cs
+++ FerretLib.SFML/ChronoEventArgs.cs
@@ -8,11 +8,13 @@ namespace FerretLib.SFML
         {
             Monotonic = monotonic;
             Delta = delta;
+            DateTime = DateTime.Now;
             Fps = fps;
         }
 
         public long Monotonic { get; private set; }
         public double Delta { get; private set; }
         public double Fps { get; private set; }
+        public DateTime DateTime { get; private set; }
     }
 }
