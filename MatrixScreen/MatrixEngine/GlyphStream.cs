f1a21db5b75c57b143e88b1a3f30de46bb58e98e
Nathan Chere <git@nathanchere.com.au>
2014-05-02 22:32:38 +1000

    revert some debug stuff

--- MatrixScreen/MatrixEngine/GlyphStream.cs
+++ MatrixScreen/MatrixEngine/GlyphStream.cs
@@ -10,8 +10,8 @@ namespace MatrixScreen
 {
     public class GlyphStream : IEntity
     {
-        private const int MAX_GLYPHS = 8;
-        private const int MIN_GLYPHS = 1;
+        private const int MAX_GLYPHS = 20;
+        private const int MIN_GLYPHS = 3;
 
         private readonly float movementRate;
         private readonly float scale;
@@ -37,10 +37,11 @@ namespace MatrixScreen
                 GetRandom.Int((int)-GlyphSize.X, (int) (_workingArea.Width + GlyphSize.X)),
                 GetRandom.Int((int)-GlyphSize.Y, (int)(_workingArea.Height + GlyphSize.Y)));
             
-            numberOfGlyphs = 5;
-            GlyphPosition = new Vector2f(10,10);
-            movementRate = 80;
-            scale = GetRandom.Float(0.1f, 0.3f);
+            // Just for debugging
+            //numberOfGlyphs = 5;
+            //GlyphPosition = new Vector2f(10,10);
+            //movementRate = 80;
+            //scale = GetRandom.Float(0.1f, 0.3f);
 
             _glyphs = new List<Glyph>();
             for (int i = 0; i < numberOfGlyphs; i++)
@@ -96,13 +97,13 @@ namespace MatrixScreen
                     0,0);
 
                 Debug.DrawRect(canvas, new Color(0, 255, 255, 20),
-                    MaskPosition.X, MaskPosition.Y,
-                    200, 10,
+                    MaskPosition.X - 5, MaskPosition.Y,
+                    GlyphSize.X + 10, 10,
                     0,5);
 
                 Debug.DrawRect(canvas, new Color(0, 255, 255, 20),
-                    MaskPosition.X, MaskPosition.Y + MaskSize.Y,
-                    200, 10,
+                    MaskPosition.X - 5, MaskPosition.Y + MaskSize.Y,
+                    GlyphSize.X + 10, 10,
                     0,5);
 
 
