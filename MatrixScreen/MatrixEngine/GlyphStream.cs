ae896416069088a66174d243014e5c42e06716f8
Nathan Chere <git@nathanchere.com.au>
2014-05-06 11:41:54 +1000

    Cleanup GlyphStream

--- MatrixScreen/MatrixEngine/GlyphStream.cs
+++ MatrixScreen/MatrixEngine/GlyphStream.cs
@@ -8,100 +8,61 @@ namespace MatrixScreen
 {
     public class GlyphStream : IEntity
     {
-        private readonly int MAX_GLYPHS;
-        private readonly int MIN_GLYPHS;
-        private readonly float MAX_MOVEMENTRATE;
-        private readonly float MIN_MOVEMENTRATE;
-        private readonly float MIN_GLYPHSCALE;
-        private readonly float MAX_GLYPHSCALE;
-        private readonly float MARGIN_SCALE;        
-
-        private readonly float movementRate;
-        private readonly float scale;
-        private readonly GlyphConfig _glyphConfig;
-        private float displayDurationMultipier; // affects how long 
+        private readonly float _movementRate;
+        private readonly float _scale;
+        private readonly float _displayDurationMultipier; // affects how long 
 
         private readonly Rectangle _workingArea;
-
-        private List<Glyph> _glyphs;
-
-        // TODO: chance of glyph change; glyph index, color
-
+        private readonly List<Glyph> _glyphs;
+        private readonly Vector2f MaskSize;
+        private readonly Vector2f GlyphSize;        
+       
         private Vector2f MaskPosition; // Stream position - scrolls down the screen
         private Vector2f GlyphPosition; // Individual glyphs location - doesn't change
         private IntRect GlyphArea; // Total glyph-occupied region - doesn't change
 
+        public bool IsExpired { get; private set; }
+
         public GlyphStream(GlyphStreamConfig settings, Rectangle workingArea)
         {
             _workingArea = workingArea;
-            _glyphConfig = settings.GlyphConfig;
 
-            MAX_GLYPHS = settings.MaxGlyphs;
-            MIN_GLYPHS = settings.MinGlyphs;
-            MAX_MOVEMENTRATE = settings.MaxMovementRate;
-            MIN_MOVEMENTRATE = settings.MinMovementRate;
-            MIN_GLYPHSCALE = settings.MinGlyphScale;
-            MAX_GLYPHSCALE = settings.MaxGlyphScale;
-            MARGIN_SCALE = settings.MarginScale;
+            var numberOfGlyphs = GetRandom.Int(settings.MinGlyphs, settings.MaxGlyphs);
 
-            movementRate = GetRandom.Float(MIN_MOVEMENTRATE, MAX_MOVEMENTRATE);
-            var numberOfGlyphs = GetRandom.Int(MIN_GLYPHS, MAX_GLYPHS);
-            scale = GetRandom.Float(MIN_GLYPHSCALE, MAX_GLYPHSCALE);
-            displayDurationMultipier = GetRandom.Float(0.5f / numberOfGlyphs, 1f);
+            _movementRate = GetRandom.Float(settings.MinMovementRate, settings.MaxMovementRate);            
+            _scale = GetRandom.Float(settings.MinGlyphScale, settings.MaxGlyphScale);
+            _displayDurationMultipier = GetRandom.Float(0.5f / numberOfGlyphs, 1f);
+
+            GlyphSize = new Vector2f(Glyph.GLYPH_WIDTH * _scale, Glyph.GLYPH_HEIGHT * _scale);
 
             GlyphPosition = new Vector2f(
-                GetRandom.Int((int)-GlyphSize.X, (int) (_workingArea.Width + GlyphSize.X)),
+                GetRandom.Int((int)-GlyphSize.X, (int)(_workingArea.Width + GlyphSize.X)),
                 GetRandom.Int((int)-GlyphSize.Y, (int)(_workingArea.Height + GlyphSize.Y)));
-
-            ////TODO: refactor to a debugGlpyhStream class
-            //if (Config.IsDebugGlyphStreams)
-            //{
-            //    // Just for debugging
-            //    numberOfGlyphs = 6;
-            //    GlyphPosition = new Vector2f(10,10);
-            //    movementRate = 80;
-            //    scale = 0.2f;
-            //    displayDurationMultipier = 0.5f;
-            //}
 
             _glyphs = new List<Glyph>();
             for (int i = 0; i < numberOfGlyphs; i++)
             {
-                var y = GlyphPosition.Y + (i * Glyph.GLYPH_HEIGHT * scale * MARGIN_SCALE);
+                var y = GlyphPosition.Y + (i * Glyph.GLYPH_HEIGHT * _scale * settings.MarginScale);
 
                 if (y + Glyph.GLYPH_HEIGHT < 0) continue;
                 if (y > workingArea.Height) continue;
 
-                _glyphs.Add(new Glyph(new Vector2f(GlyphPosition.X, y), scale, _glyphConfig));
+                _glyphs.Add(new Glyph(new Vector2f(GlyphPosition.X, y), _scale, settings.GlyphConfig));
             }
 
             MaskPosition = new Vector2f(GlyphPosition.X, GlyphPosition.Y - MaskSize.Y);
+            MaskSize = new Vector2f(GlyphSize.X, GlyphSize.Y * _glyphs.Count * _displayDurationMultipier); // TODO: incorporate margin
             
+        
             GlyphArea = new IntRect(
                 (int)GlyphPosition.X,
                 (int)GlyphPosition.Y,
                 (int)GlyphSize.X,
-                (int)GlyphSize.Y + (int)(GlyphSize.Y * MARGIN_SCALE * (_glyphs.Count - 1f))
-                );        
+                (int)GlyphSize.Y + (int)(GlyphSize.Y * settings.MarginScale * (_glyphs.Count - 1f))
+                );
         }
 
-
-        public Vector2f MaskSize
-        {
-            get { return new Vector2f(GlyphSize.X, GlyphSize.Y * _glyphs.Count * displayDurationMultipier); } //TODO: handle margin scale
-        }
-
-        public Vector2f GlyphSize
-        {
-            get
-            {
-                return new Vector2f(Glyph.GLYPH_WIDTH * scale, Glyph.GLYPH_HEIGHT * scale);
-            }
-        }
-
-        public bool IsExpired { get; private set; }
-
-        public IntRect MaskArea()
+        private IntRect MaskArea()
         {
             return new IntRect(
                 (int)MaskPosition.X,
@@ -109,41 +70,19 @@ namespace MatrixScreen
                 (int)MaskSize.X,
                 (int)MaskSize.Y
                 );
-        }        
+        }
 
         public void Render(RenderTarget canvas)
         {
-            _glyphs.ForEach(g=>g.Render(canvas));
-
-            ////TODO: refactor to a debugGlyphStream class
-            //if (Config.IsDebugRendering) // debug
-            //{
-            //    //Debug.DrawRect(canvas, new Color(0, 255, 0, 20),
-            //    //    MaskPosition.X, MaskPosition.Y,
-            //    //    MaskSize.X, MaskSize.Y,
-            //    //    //Glyph.GLYPH_WIDTH * 0.5f * scale,0);
-            //    //    0,0);
-
-            //    Debug.DrawRect(canvas, new Color(0, 255, 255, 20),
-            //        MaskPosition.X - 5, MaskPosition.Y,
-            //        GlyphSize.X + 10, 10,
-            //        0,5);
-
-            //    Debug.DrawRect(canvas, new Color(0, 255, 255, 20),
-            //        MaskPosition.X - 5, MaskPosition.Y + MaskSize.Y,
-            //        GlyphSize.X + 10, 10,
-            //        0,5);
-
-
-            //}
+            _glyphs.ForEach(g => g.Render(canvas));
         }
 
         public void Update(ChronoEventArgs chronoArgs)
         {
             if (IsExpired) return;
 
-            MaskPosition.Y += (float)(movementRate * chronoArgs.Delta);
-            _glyphs.ForEach(g=>g.Update(chronoArgs, MaskArea()));
+            MaskPosition.Y += (float)(_movementRate * chronoArgs.Delta);
+            _glyphs.ForEach(g => g.Update(chronoArgs, MaskArea()));
 
             CheckIfExpired();
         }
